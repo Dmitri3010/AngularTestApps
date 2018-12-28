b277fb8a084a472e8776fd014cd4b412375ecff9
dmitri <lipov>
2018-12-28 19:20:40 +0300

    Add settings page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,26 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }   from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings.component';
+import { HomeComponent } from './home.component';
+
+
+const appRoutes: Routes = [
+  { path: 'settings', component: SettingsComponent},
+  { path: 'home', component: HomeComponent},
+  {
+  path: '**',
+    redirectTo: 'home',
+  pathMatch: 'full'
+ }
+];
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, SettingsComponent, HomeComponent ],
+  bootstrap:    [ AppComponent, HomeComponent, SettingsComponent ]
 })
 export class AppModule { }
+
---
+++ src/app/home.component.ts
@@ -0,0 +1,41 @@
+import {Component} from '@angular/core';
+
+class Item {
+  purchase: string;
+  done: boolean;
+  price: number;
+  count: number;
+
+  constructor(purchase: string, price: number, count: number) {
+
+    this.purchase = purchase;
+    this.price = price;
+    this.count = count;
+    this.done = false;
+
+  }
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class HomeComponent {
+  items: Item[] =
+    [
+      {purchase: 'Хлеб', done: false, price: 15.9, count: 1},
+      {purchase: 'Масло', done: false, price: 60, count: 10},
+      {purchase: 'Картофель', done: true, price: 22.6, count: 14},
+      {purchase: 'Сыр', done: false, price: 310, count: 11},
+      {purchase: 'Саня', done: false, price: 30, count: 1}
+    ];
+
+  addItem(text: string, price: number, count: number): void {
+
+    if (!text || text.trim() === '' || !price) {
+      return;
+    }
+    this.items.push(new Item(text, price, count));
+  }
+}
